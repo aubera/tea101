1556745cb9b8220376489a644a4337b81e89a8ac
Andor Auber <andor.auber@gmail.com>
2019-09-03 21:00:18 +0200

    feat(Description.vue): add Description comp. to show selected tea desc

---
+++ src/components/Description.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="description">
+    <p>{{ tea }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Description",
+  props: {
+    tea: String
+  }
+};
+</script>
+
+<style scoped lang="sass">
+.description
+  min-height: 550px
+  width: 67%
+
+  p
+    text-align: justify
+</style>
